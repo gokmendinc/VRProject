c107390a8aea4bb8887488fb3f3d5743403b7038
Gmen <57567816+gokmendinc@users.noreply.github.com>
2026-04-15 16:39:25 +0300

    s

---
+++ Assets/GravityBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using static UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics.HapticsUtility;
+
+public class GravityBehaviour : MonoBehaviour
+{
+    public CharacterController characterController;
+    float gravity = -9.81f;
+    Vector3 velocity;
+    void Update()
+    {
+
+        // 🔴 ADD IT HERE
+        if (characterController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -5f; // stick to ground
+        }
+
+        // apply gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        // 🔴 OPTIONAL BUT VERY IMPORTANT (helps your exact issue)
+        characterController.Move(Vector3.down * 0.05f);
+    }
+}
